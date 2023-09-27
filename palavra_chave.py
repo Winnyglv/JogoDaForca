a709943fa9713c04dc333c94a0b39a537dd35e91
Winny Gonçalves <110840098+Winnyglv@users.noreply.github.com>
2023-09-27 16:49:13 -0300

    Add files via upload

---
+++ palavra_chave.py
@@ -0,0 +1,7 @@
+
+import random
+
+palavra = ['camelo', 'arara', 'cobra', 'bagre', 'arraia', 'vendedora', 'dentista', 'biologa', 'cirurgiao', 'arquiteta', 'jornalista',
+'advogada', 'melancia', 'maça', 'mexerica', 'framboesa', 'abacaxi', 'banana', 'mexerica', 'cereja', 'goiaba']
+
+
